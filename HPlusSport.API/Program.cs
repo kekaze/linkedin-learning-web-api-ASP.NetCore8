3fa3558366ac04f65b12399f496ba7a5d75bbc0c
kekaze <jroyceyanson@gmail.com>
2024-09-16 13:48:37 +0800

    Added minimal API based on the existing http-based API

--- HPlusSport.API/Program.cs
+++ HPlusSport.API/Program.cs
@@ -54,11 +54,64 @@ app.MapGet("/products/{id}", async (int id, ShopContext _context) =>
         return Results.NotFound();
     }
     return Results.Ok(product);
-});
+}).WithName("GetProduct");
 
 app.MapGet("/products/available", async (ShopContext _context) =>
 {
     return await _context.Products.Where(product => product.IsAvailable).ToArrayAsync();
+});
+
+app.MapPost("/products", async (ShopContext _context, Product product) =>
+{
+    if (string.IsNullOrEmpty(product.Name)) // custom vallidation
+    {
+        return Results.BadRequest("Product name is required.");
+    }
+
+    _context.Products.Add(product);
+    await _context.SaveChangesAsync();
+
+    return Results.CreatedAtRoute(
+    "GetProduct",
+    new { id = product.Id },
+    product);
+});
+
+app.MapPut("/products", async (int id, ShopContext _context, Product product) =>
+{
+    if (id != product.Id) // if the id on the route is not the id on the payload
+    {
+        return Results.BadRequest();
+    }
+
+    _context.Entry(product).State = EntityState.Modified;
+
+    try
+    {
+        await _context.SaveChangesAsync();
+    } catch (DbUpdateConcurrencyException) // looking for concurrency issues i.e. a parallel call changed something on the database for the same product
+    {
+        if (!_context.Products.Any(p => p.Id == id))
+        {
+            return Results.NotFound();
+        } else
+        {
+            throw; // server error
+        }
+    }
+    return Results.NoContent();
+});
+app.MapDelete("/products/{id}", async (int id, ShopContext _context) =>
+{
+    var product = await _context.Products.FindAsync(id);
+            if (product == null)
+            {
+                return Results.NotFound();
+            }
+            _context.Products.Remove(product);
+            await _context.SaveChangesAsync();
+
+            return Results.Ok(product);
 });
 
 app.Run();
